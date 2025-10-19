897917f08f6d32e6965f62dba06b8a071fc6bc24
Lukas Ružauskas <nequadora@gmail.com>
2025-10-19 10:38:25 +0300

    trailinSlash true for static pages

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig = {
   images: { unoptimized: true },
   basePath: isProd ? "/deadlockchallenge" : "",
   assetPrefix: isProd ? "/deadlockchallenge/" : "",
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
