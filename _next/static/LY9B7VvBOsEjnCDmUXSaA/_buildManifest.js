cc7eb9159645e30237c97704e56b510548a67678
Lukas Ružauskas <nequadora@gmail.com>
2025-10-19 10:26:45 +0300

    Updates

---
+++ _next/static/LY9B7VvBOsEjnCDmUXSaA/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/01d3d78462c55751.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/deadlockchallenge//_next/:path+",
+        "destination": "/deadlockchallenge/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
