ea1698159dfc3119bb378c5d4e52e2dac18f5b90
Lukas Ružauskas <nequadora@gmail.com>
2025-10-19 12:22:55 +0300

    removed deadlock challenge for vercel

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
   images: { unoptimized: true },
-  basePath: isProd ? "/deadlockchallenge" : "",
-  assetPrefix: isProd ? "/deadlockchallenge/" : "",
+  basePath: "",
+  assetPrefix: "",
   trailingSlash: true,
 };
 
