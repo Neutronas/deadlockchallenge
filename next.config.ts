5aaf156e83f8c9d123e81712e9625d7d645d8155
Lukas Ružauskas <nequadora@gmail.com>
2025-10-19 10:27:18 +0300

    to deploy on gh pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: "export", // enables static HTML export
+  images: { unoptimized: true }, // disable Next image optimization
+  basePath: "/deadlockchallenge", // your repo name here
+  assetPrefix: "/deadlockchallenge/",
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
